bf70b439b5189f8024dbf4cf2bf42d08bd81a5eb
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-22 21:13:30 +0100

    minor linter error fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,29 +8,27 @@ const Contact = () => {
       <div className="container">
         <h2 className="title is-3 is-centered">Contact</h2>
 
-        <div className="columns is multiline">
-          <div className="column is-three-quarters-desktop">
-            <p>Please contact me below</p>
+        <div className="column is-three-quarters-desktop">
+          <p>Please contact me below</p>
 
-            <div className="column is-full-desktop ">
-              <img  className="icon" src="./assets/gmail.png"/>
-              <a href="mailto:mrarjunmodi@gmail.com">mrarjunmodi@gmail.com</a>
-            </div>
-
-            <div className="column is-full-desktop ">
-              <img  className="icon" src="./assets/github2.png"/>
-              <a href="https://github.com/arj8728/" target="_blank" rel="noopener noreferrer">  Arjun Modi Github  </a>
-            </div>
-
-            <div className="column is-full-desktop ">
-              <img  className="icon" src="./assets/linkedin.png"/>
-              <a href="https://www.linkedin.com/in/arjun-n-modi/" target="_blank" rel="noopener noreferrer">  Arjun Modi LinkedIn  </a>
-            </div>
-
+          <div className="column is-full-desktop ">
+            <img  className="icon" src="./assets/gmail.png"/>
+            <a href="mailto:mrarjunmodi@gmail.com">mrarjunmodi@gmail.com</a>
           </div>
-        </div>
 
+          <div className="column is-full-desktop ">
+            <img  className="icon" src="./assets/github2.png"/>
+            <a href="https://github.com/arj8728/" target="_blank" rel="noopener noreferrer">  Arjun Modi Github  </a>
+          </div>
+
+          <div className="column is-full-desktop ">
+            <img  className="icon" src="./assets/linkedin.png"/>
+            <a href="https://www.linkedin.com/in/arjun-n-modi/" target="_blank" rel="noopener noreferrer">  Arjun Modi LinkedIn  </a>
+          </div>
+
+        </div>
       </div>
+
     </section>
   )
 }
