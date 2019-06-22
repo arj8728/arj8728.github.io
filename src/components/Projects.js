b31efbb6cf43b07cb392f8829bdb9dcddddbe28e
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-22 11:57:16 +0100

    project links open new window

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,32 +13,32 @@ const Projects = () => {
           <div className="column is-two-fifths">
             <figure className="image textBlur">
               <img className="snapshot" src="./assets/project4.jpg" />
-              <h2 className="title"><a href="https://github.com/arj8728/architech">  Project Four Github Link </a></h2>
-              <h3 className="title"><a href="https://architech-hero.herokuapp.com/">  Architech Link </a></h3>
+              <h2 className="title"><a href="https://github.com/arj8728/architech" target="_blank" rel="noopener noreferrer">  Project Four Github Link </a></h2>
+              <h3 className="title"><a href="https://architech-hero.herokuapp.com/" target="_blank" rel="noopener noreferrer">  Architech Link </a></h3>
             </figure>
           </div>
 
           <div className="column is-two-fifths">
             <figure className="image textBlur">
               <img className="snapshot" src="./assets/project3.jpg" />
-              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-3-NerdCrate"> Project Three Github Link </a></h2>
-              <h3 className="title"><a href="https://nerdcrate.herokuapp.com/">  NerdCrate Link </a></h3>
+              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-3-NerdCrate" target="_blank" rel="noopener noreferrer"> Project Three Github Link </a></h2>
+              <h3 className="title"><a href="https://nerdcrate.herokuapp.com/" target="_blank" rel="noopener noreferrer">  NerdCrate Link </a></h3>
             </figure>
           </div>
 
           <div className="column is-two-fifths">
             <figure className="image textBlur">
               <img className="snapshot" src="./assets/project2.jpg" />
-              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-2-React-Hackathon"> Project Two Github Link </a></h2>
-              <h3 className="title"><a href="https://arj8728.github.io/SEI-Project-2-React-Hackathon/#/">  Cycle In London Link </a></h3>
+              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-2-React-Hackathon" target="_blank" rel="noopener noreferrer"> Project Two Github Link </a></h2>
+              <h3 className="title"><a href="https://arj8728.github.io/SEI-Project-2-React-Hackathon/#/" target="_blank" rel="noopener noreferrer">  Cycle In London App Link </a></h3>
             </figure>
           </div>
 
           <div className="column is-two-fifths">
             <figure className="image textBlur">
               <img className="snapshot" src="./assets/project1.jpg" />
-              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-01-Snake-Game"> Project One Github Link </a></h2>
-              <h3 className="title"><a href="https://arj8728.github.io/SEI-Project-01-Snake-Game/">  Super Mario Snake Link </a></h3>
+              <h2 className="title"><a href="https://github.com/arj8728/SEI-Project-01-Snake-Game" target="_blank" rel="noopener noreferrer"> Project One Github Link </a></h2>
+              <h3 className="title"><a href="https://arj8728.github.io/SEI-Project-01-Snake-Game/" target="_blank" rel="noopener noreferrer">  Super Mario Snake Link </a></h3>
 
             </figure>
           </div>
