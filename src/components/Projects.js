a1292ce579e9b76c6b317e42d0bcd91d83169d30
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-22 10:22:31 +0100

    worked out how to style each project with a blur effect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,31 +10,27 @@ const Projects = () => {
         <p className="title is-5 is-centered">These are the projects I have completed at GA</p>
         <div className="columns is-multiline has-text-centered is-centered">
 
-          <h2 title is-5 is-centered>Project Four</h2>
           <div className="column is-two-fifths">
             <img className="snapshot" src="./assets/project4.jpg" />
           </div>
 
-          <h2>Project Three</h2>
+
           <div className="column is-two-fifths">
             <img className="snapshot" src="./assets/project3.jpg" />
           </div>
 
-          <h2>Project Two</h2>
           <div className="column is-two-fifths">
             <img className="snapshot" src="./assets/project2.jpg" />
           </div>
 
-          <h2>Project One</h2>
           <div className="column is-two-fifths">
-            <img className="snapshot" src="./assets/project1.jpg" />
-
-
+            <figure className="image textBlur">
+              <img className="snapshot" src="./assets/project1.jpg" />
+              <h2 className="title">Project One</h2>
+            </figure>
           </div>
 
         </div>
-
-
 
       </div>
 
