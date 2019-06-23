84b3c862a85c6a764730fff6880e8ced84623b0a
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-23 20:19:46 +0100

    minor styling on the contact icons and links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,19 +11,19 @@ const Contact = () => {
         <div className="column is-three-quarters-desktop">
           <p>Please contact me below</p>
 
-          <div className="column is-full-desktop ">
+          <div className="column is-full-desktop is-two-fifths">
             <img  className="icon" src="./assets/gmail.png"/>
-            <a href="mailto:mrarjunmodi@gmail.com">mrarjunmodi@gmail.com</a>
+            <a href="mailto:mrarjunmodi@gmail.com" className="contactlink">mrarjunmodi@gmail.com</a>
           </div>
 
           <div className="column is-full-desktop ">
             <img  className="icon" src="./assets/github2.png"/>
-            <a href="https://github.com/arj8728/" target="_blank" rel="noopener noreferrer">  Arjun Modi Github  </a>
+            <a href="https://github.com/arj8728/" target="_blank" rel="noopener noreferrer" className="contactlink">  Arjun Modi Github  </a>
           </div>
 
           <div className="column is-full-desktop ">
             <img  className="icon" src="./assets/linkedin.png"/>
-            <a href="https://www.linkedin.com/in/arjun-n-modi/" target="_blank" rel="noopener noreferrer">  Arjun Modi LinkedIn  </a>
+            <a href="https://www.linkedin.com/in/arjun-n-modi/" target="_blank" rel="noopener noreferrer" className="contactlink">  Arjun Modi LinkedIn  </a>
           </div>
 
         </div>
