a69ccbfb999f0a622b90ad4fb0e4fb006ddaf423
Arjun Modi <mrarjunmodi@gmail.com>
2019-06-25 21:26:35 +0100

    changed background added about me section changed project link colours to red and black highlight

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,111 +3,141 @@ import React from 'react'
 const Skills = () => {
   return(
     <section id="skills">
-      <div className="container">
-        <h2 className="title is-3 is-centered">Tech Stack</h2>
-        <p>I am a Junior Software Engineer, below are my skills! I used to work as a scientist but developed an interest in software and computing. This led me to a software engineering course at General Assembly (GA). After I completed my course at GA I started  learning Java and Ruby on Codecademy. </p>
-      </div>
 
       <div className="container">
 
-        <div className="column is-full-mobile is-half-desktop">
-          <h2 className="title is-3 is-centered techtitle">My Tech stack:</h2>
+        <h2 className="title is-3 is-centered">Skills</h2>
+        <div className="columns">
+
 
           <div className="columns is-multiline is-mobile">
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/html5.png" />
-              <div className="name">HTML5</div>
-            </div>
+            <div className="column is-full-mobile is-half-desktop">
+              <div className="columns is-multiline is-mobile">
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/html5.png" />
+                  <div className="name">HTML5</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/css3.png" />
-              <div className="name">CSS3</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/css3.png" />
+                  <div className="name">CSS3</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/javascript.png" />
-              <div className="name">JavaScript</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/javascript.png" />
+                  <div className="name">JavaScript</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/react.png" />
-              <div className="name">ReactJS</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/react.png" />
+                  <div className="name">ReactJS</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/nodejs.png" />
-              <div className="name">Node.js</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/nodejs.png" />
+                  <div className="name">Node.js</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/mongodb.png" />
-              <div className="name">MongoDB</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/mongodb.png" />
+                  <div className="name">MongoDB</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/python.png" />
-              <div className="name">Python</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/python.png" />
+                  <div className="name">Python</div>
+                </div>
 
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/postgresql.png" />
-              <div className="name">Node.js</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/postgresql.png" />
+                  <div className="name">PostgreSQL</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/npm.png" />
-              <div className="name">npm</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/npm.png" />
+                  <div className="name">npm</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/git.png" />
-              <div className="name">Git</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/git.png" />
+                  <div className="name">Git</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/github.png" />
-              <div className="name">GitHub</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/github.png" />
+                  <div className="name">GitHub</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/heroku.png" />
-              <div className="name">Heroku</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/heroku.png" />
+                  <div className="name">Heroku</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/mysql.png" />
-              <div className="name">MySQL</div>
-            </div>
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/mysql.png" />
+                  <div className="name">MySQL</div>
+                </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/jquery.png" />
-              <div className="name">jQuery</div>
+
+                <div className="column is-one-third-mobile is-one-quarter-desktop">
+                  <img className="logo" src="./assets/jquery.png" />
+                  <div className="name">jQuery</div>
+                </div>
+              </div>
+
+
+              <h2 className="title is-3 is-centered techtitle">Currently learning:</h2>
+              <div className="columns is-multiline  is-mobile">
+                <div className="column is-one-third-mobile is-one-fifth-desktop">
+                  <img className="logo" src="./assets/java.png" />
+                  <div className="name">Java</div>
+                </div>
+
+                <div className="column is-one-third-mobile is-one-fifth-desktop">
+                  <img className="logo" src="./assets/ruby.png" />
+                  <div className="name">Ruby</div>
+                </div>
+
+                <div className="column is-one-third-mobile is-one-fifth-desktop">
+                  <img className="logo" src="./assets/bootstrap.png" />
+                  <div className="name">Bootstrap</div>
+                </div>
+              </div>
             </div>
           </div>
 
+          <div className="column is-full-mobile is-half-desktop">
+            <div className="columns is-multiline is-mobile">
+              <h2 className="title is-3 is-centered">About me:</h2>
+              <p>
+                <br/>
+              Having worked as a biomedical scientist for around 9 years I decided that I wanted a career change.
 
-          <h2 className="title is-3 is-centered techtitle">Currently learning:</h2>
-          <div className="columns is-multiline  is-mobile">
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/java.png" />
-              <div className="name">Java</div>
+                <br/>
+                <br/>
+              Throughout my career I have enjoyed using programs and software to solve various problems that I have encountered in scientific dissertations, to researching important clinical questions in cancer research. In my previous role I was analysing data and putting together reports based on relationships between cancer growth and biomarkers. This led me to gaining a deep interest in how data could be stored and visualised by using advanced computer programming languages.
+
+                <br/>
+                <br/>
+
+                I came to GA to realise that goal of storing data and producing complex visual representations of this data in the form of websites. My skills as a molecular biologist have helped me in this course as I was able to apply my analytical and experimental experience into making websites and putting together solutions for complex JavaScript problems.
+
+                <br/>
+                <br/>
+
+                I hope to join this exciting software engineering field as soon as possible.
+
+              </p>
             </div>
 
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/ruby.png" />
-              <div className="name">Ruby</div>
-            </div>
-
-            <div className="column is-one-third-mobile is-one-fifth-desktop">
-              <img className="logo" src="./assets/bootstrap.png" />
-              <div className="name">Bootstrap</div>
-            </div>
           </div>
-
         </div>
 
       </div>
+
+
+
 
     </section>
   )
